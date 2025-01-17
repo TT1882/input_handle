567ccb583cb2ef50cc2addc7c6b6ab5186559889
Luke Wilkinson <wilkinsonluke@proton.me>
2025-01-17 21:05:24 +0000

    new file:   .gitignore 	new file:   Cargo.toml 	new file:   README.md 	new file:   src/lib.rs

---
+++ README.md
@@ -0,0 +1,41 @@
+import-handler is a basic library designed to handle user inputs of any type and avoid a panic. It has error handling built in
+
+# Current functions
+### get_u8_input
+### get_u32_input
+### get_u64_input
+### get_u128_input
+### get_usize_input
+### get_i8_input
+### get_i32_input
+### get_i64_input
+### get_i128_input
+### get_isize_input
+### get_f32_input
+### get_f64_input
+### get_bool_input
+### get_char_input
+### get_string_input
+
+# How it works
+
+Each function follows the same principles, adjusting for each data type. Here is the code for get_u32_input:
+
+```rust
+pub fn get_u32_input(message: &str) -> u32 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<u32>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+```
+
+The function takes in &str data "message", which is the message displayed before the input. The input is then read and checked if it can be converted to u32. If it can, the value is returned, if not an error is displayed and the input variable is reset then a new input is gathered.
---
+++ src/lib.rs
@@ -0,0 +1,228 @@
+#![allow(non_snake_case)]
+
+use std::io;
+fn main(){}
+pub fn get_u8_input(message: &str) -> u8 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<u8>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_u32_input(message: &str) -> u32 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<u32>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_u64_input(message: &str) -> u64 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<u64>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_u128_input(message: &str) -> u128 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<u128>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_usize_input(message: &str) -> usize {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<usize>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_i8_input(message: &str) -> i8 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<i8>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_i32_input(message: &str) -> i32 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<i32>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_i64_input(message: &str) -> i64 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<i64>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_i128_input(message: &str) -> i128 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<i128>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_isize_input(message: &str) -> isize {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<isize>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_f32_input(message: &str) -> f32 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<f32>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_f64_input(message: &str) -> f64 {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<f64>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_bool_input(message: &str) -> bool {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<bool>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_char_input(message: &str) -> char {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<char>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
+
+pub fn get_string_input(message: &str) -> String {
+    println!("{}", message);
+    let mut userInput = String::new();
+    loop {
+        io::stdin().read_line(&mut userInput).expect("Failed to read line");
+        match userInput.trim().parse::<String>() {
+            Ok(value) => {return value;}
+            Err(e) => {
+                println!("Invalid input! Error: {}", e);
+                userInput = String::new();
+            }
+        }
+    }
+} 
